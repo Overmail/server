524bd30133035a96887c1178f9702db63296add6
Julius Vincent Babies <julius@familie-babies.de>
2025-09-22 11:45:38 +0200

    feat: Enhance HTML response for emails with additional JavaScript for link handling and resize notifications

--- src/main/kotlin/api/mail/item/content/html/GetMailHtmlContent.kt
+++ src/main/kotlin/api/mail/item/content/html/GetMailHtmlContent.kt
@@ -16,14 +16,39 @@ fun Route.getMailHtmlContent() {
             val mail = call.getMail() ?: return@get
             val rawHtml = call.queryParameters["raw"]?.toBoolean() ?: false
 
+            val html = mail.htmlBody?.plus("\n" + JS_INSET)
+
             if (rawHtml) {
-                if (mail.htmlBody != null) call.response.cacheControl(CacheControl.MaxAge(maxAgeSeconds = 86400))
-                call.respondText(mail.htmlBody.orEmpty(), ContentType.Text.Html)
+                if (html != null) call.response.cacheControl(CacheControl.MaxAge(maxAgeSeconds = 86400))
+                call.respondText(html.orEmpty(), ContentType.Text.Html)
             }
-            else call.respond(MailHtmlContentResponse(mail.htmlBody))
+            else call.respond(MailHtmlContentResponse(html))
         }
     }
 }
+
+private const val JS_INSET = """
+<script>
+  const links = document.getElementsByTagName('a');
+  for (let i = 0; i < links.length; i++) {
+    links[i].setAttribute('target', '_blank');
+    links[i].setAttribute('rel', 'noopener noreferrer');
+  }
+  
+  function notifyResize() {
+    const height = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight
+    );
+    parent.postMessage({ type: 'resize', height }, '*');
+  }
+            
+  window.addEventListener('load', notifyResize);
+  new MutationObserver(notifyResize).observe(document.body, {
+    childList: true, subtree: true, attributes: true
+  });
+</script>
+"""
 
 @Serializable
 data class MailHtmlContentResponse(
